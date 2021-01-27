8cf3dd28a30efe2feb9ed9af325194c6b30cb27d
gqadirdepar <gqadirdepar@gmail.com>
2021-01-27 16:21:39 +0500

    coding updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div >
-      This is first react project - 01
-    </div>
-  );
-}
-
-export default App;
+import React ,{useState} from 'react'
+import Message from './Message'
+export default  function App() {  
+  let [count,setcount]= useState(2);
+   return (
+    <div >      
+      This is first react project - 01    
+      <Message counter={count} />
+   
+    <button onClick={
+      ()=>setcount(++count)
+      }
+      > Update Counter </button>
+    </div> 
+     ); 
+    
+    }  
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function message(props){
+    return(
+        <h3> The value of counter is {props.counter} </h3>
+    )
+}
